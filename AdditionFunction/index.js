25b8ba31ced8941c7a65e1598c182e191951994b
Naseer Yafai <naseer_yafai@hotmail.com>
2024-05-13 22:21:59 +0100

    Created function to add two values!

---
+++ AdditionFunction/index.js
@@ -0,0 +1,9 @@
+function calculateSum(firstSum, secondSum) {
+    
+    let finalSum = firstSum + secondSum
+    return finalSum    
+}
+
+console.log(calculateSum(7, -2))
+const result = calculateSum(3, -2)
+console.log(result)
